0cfcf8cb018e80a75300468eefc515fb88ec6461
estefany234 <meamaya@seas.upenn.edu>
2024-01-07 13:11:01 -0500

    Create RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,28 @@
+public class RewardValue {
+    private final double cashValue;
+    private final double milesToCashConversionRate = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private int convertToMiles(double cashValue) {
+        return (int) (cashValue / milesToCashConversionRate);
+    }
+
+    private double convertToCash(int milesValue) {
+        return milesValue * milesToCashConversionRate;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
+}
